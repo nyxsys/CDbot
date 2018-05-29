7f4ff3d51b1f702697b2f4b2e090d12df47e5430
Mark Barbera (Collabera) <v-barbm@microsoft.com>
2018-05-29 09:54:33 -0700

    init commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=mb02-botbot-Web-Deploy.pubxml -p:Password=5i2QGhd2iGee8QqEyvEy9i6nKfjLqm3i0NdLHqi1uEF7L3yfyAYGzBfYC02C
+
